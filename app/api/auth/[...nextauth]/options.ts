f495ee692e0f03166b3a4f738b5d6c202e78aedb
jahzlariosa <dev@josephlariosa.com>
2023-07-08 02:06:51 +0800

    Strapi Integration

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -8,4 +8,37 @@ export const options: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+
+  // Integrate to Strapi
+  session: { strategy: "jwt" },
+  callbacks: {
+    async session({ user, session, token }) {
+        session.user = token as any;
+        /* @ts-ignore */
+        session.user.id = user ? user.id : null;
+        return Promise.resolve(session);
+    },
+    
+    async jwt({ token, user, account }) {
+        const isSignIn = user ? true : false;
+        if (isSignIn && account) {
+            try {
+                console.log("Google Account >>>>>>>>>>>>>> ", account);
+                const public_url = process.env.STRAPI_PUBLIC_API_URL;
+                const response = await fetch(
+                    `${public_url}/api/auth/${account.provider}/callback?access_token=${account?.access_token}`
+                );
+                const data = await response.json();
+                console.log("Strapi Callback Data >>>>>>>>>>>>>> ", data);
+                token.jwt = data.jwt;
+                token.id = data.user.id;
+            } catch (error) {
+                console.error('Fetch failed:', error);
+            }
+        }
+        return Promise.resolve(token);
+    },
+},
+secret: process.env.NEXTAUTH_SECRET as string
+
 };
