4d7d9e3b145bd16a47b3b7ae4547c845c5a6ced7
jahzlariosa <dev@josephlariosa.com>
2023-07-11 01:03:28 +0800

    Small Change

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 export default function Navbar() {
     return (
-        <nav className="bg-zinc-800 p-4">
+        <nav className="bg-white p-4">
             <div className="container mx-auto">
                 <div className="flex justify-between">
                     <div className="logo text-2xl">
