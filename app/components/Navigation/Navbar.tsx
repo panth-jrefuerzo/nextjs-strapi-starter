9db731cba89ac99313673f30c8cd62b9e398205a
jahzlariosa <dev@josephlariosa.com>
2023-07-13 01:56:24 +0800

    Navbar Update

--- app/components/Navigation/Navbar.tsx
+++ app/components/Navigation/Navbar.tsx
@@ -1,14 +1,15 @@
 "use client"
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
+
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { MdComputer, MdDashboard, MdHomeFilled } from 'react-icons/md';
-import ProfileDropDown from './ProfileDropDown';
-import Image from 'next/image';
-import UserNavLink from './UserNavLink';
 import { PiUserBold } from 'react-icons/pi';
-import SignOutButton from '@/app/auth/signout';
 
+import ProfileDropDown from './ProfileDropDown';
+import UserNavLink from './UserNavLink';
+import SignOutButton from '@/app/auth/signout';
 
 interface NavItemProps {
   href: string;
@@ -16,17 +17,32 @@ interface NavItemProps {
   icon: React.ElementType;
 }
 
-
-
 const Navbar: React.FC = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const mobileMenuRef = useRef<HTMLDivElement>(null);
+
   const toggleMobileMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen);
 
-  const navStyle = `sticky top-0 transition-all duration-300 dark:text-white backdrop-filter backdrop-blur-lg bg-opacity-50 border-b-[1px] border-slate-500 border-opacity-50 py-4 px-4`;
+  const navStyle = `${isMobileMenuOpen ? 'absolute opacity-0 translate-x-[-100%]' : 'sticky top-0'} transition-all duration-300 dark:text-white backdrop-filter backdrop-blur-lg bg-opacity-50 border-b-[1px] border-slate-500 border-opacity-50 py-4 px-4`;
   const menuStyle = `flex items-baseline space-x-4 gap-4 font-medium text-slate-300 ${isMobileMenuOpen ? '' : ''}`;
-  const mobileMenuStyle = `${isMobileMenuOpen ? 'opacity-100 translate-x-0 bg-slate-800 w-auto' : 'opacity-0 translate-x-[-100%]'} left-0 top-0 h-screen absolute transform transition-all duration-400 ease-in-out z-0`;
+  const mobileMenuStyle = `${isMobileMenuOpen ? 'opacity-100 translate-x-0 bg-slate-800 w-auto sticky top-0' : 'opacity-0 translate-x-[-100%] '} text-slate-300 font-medium pb-8 left-0 top-0 absolute transform transition-all duration-400 ease-in-out z-0`;
 
-  
+  useEffect(() => {
+    const handleOutsideClick = (event: MouseEvent) => {
+      if (mobileMenuRef.current && !mobileMenuRef.current.contains(event.target as Node)) {
+        setIsMobileMenuOpen(false);
+      }
+    };
+
+    if (isMobileMenuOpen) {
+      document.addEventListener('mousedown', handleOutsideClick);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, [isMobileMenuOpen]);
+
   const MenuItems = () => (
     <>
       <NavItem href="/" label="Home" icon={MdHomeFilled} />
@@ -43,48 +59,55 @@ const Navbar: React.FC = () => {
     </div>
   );
 
-
   return (
-    <nav className={navStyle}>
-      <div className="flex items-center justify-between h-12 container mx-auto container sm:px-4 lg:px-[8rem]">
-        <div className="text-left pr-6 border-r-[1px] border-gray-700">
-          <Link href="/" className={`transition-all duration-300 text-4xl font-bold `}>
-            <Image className='my-4 invert' src="/vercel.svg" width={150} height={80} alt="logo" />
-          </Link>
-        </div>
-        <div className="flex md:hidden">
-          <button type="button" className={`z-40`} onClick={toggleMobileMenu}>
-            <span className="sr-only">Open sidebar</span>
-            <GiHamburgerMenu className="inline" size={32} />
-          </button>
-        </div>
-        <div className="hidden md:flex items-center">
-          <div className={menuStyle}>
-            <MenuItems />
-            <ProfileDropDown />
+    <>
+      <nav className={navStyle}>
+        <div className="flex items-center justify-between h-12 container mx-auto container sm:px-4 lg:px-[8rem]">
+          <div className="text-left pr-6 border-r-[1px] border-gray-700">
+            <Link href="/" className={`transition-all duration-300 text-4xl font-bold `}>
+              <Image className='my-4 invert' src="/vercel.svg" width={150} height={80} alt="logo" />
+            </Link>
+          </div>
+          <div className="flex md:hidden">
+            <button type="button" className={`z-40`} onClick={toggleMobileMenu}>
+              <span className="sr-only">Open sidebar</span>
+              <GiHamburgerMenu className="inline" size={32} />
+            </button>
+          </div>
+          <div className="hidden md:flex items-center">
+            <div className={menuStyle}>
+              <MenuItems />
+              <ProfileDropDown />
+            </div>
           </div>
         </div>
-      </div>
-      <div className={mobileMenuStyle}>
+      </nav>
+      <div ref={mobileMenuRef} className={mobileMenuStyle}>
         <div className="px-5 pt-2 space-y-6 flex-block">
+          <div className="flex md:hidden absolute right-4 top-6">
+            <button type="button" className={`z-40`} onClick={toggleMobileMenu}>
+              <span className="sr-only">Open sidebar</span>
+              <GiHamburgerMenu className="inline" size={32} />
+            </button>
+          </div>
           <div className="border-b-[1px] border-slate-500">
             <Link href="/" className={`transition-all duration-300 text-4xl font-bold border-b-[1px] border-slate-500`}>
               <Image className='my-4 invert mx-auto' src="/next.svg" width={150} height={80} alt="logo" />
             </Link>
           </div>
           <UserNavLink />
-            <div className=''>
-              <Link className="mr-2" href={`/profiles/`}>
-                <PiUserBold size={20} className="inline mr-4" /> My Account
-              </Link>
-            </div>
+          <div className=''>
+            <Link className="mr-2" href={`/profiles/`}>
+              <PiUserBold size={20} className="inline mr-4" /> My Account
+            </Link>
+          </div>
           <MenuItems />
           <div>
             <SignOutButton />
           </div>
         </div>
       </div>
-    </nav>
+    </>
   );
 };
 
