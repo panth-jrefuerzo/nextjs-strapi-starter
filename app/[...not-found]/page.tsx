d9dd2b9408bb68a78591e34872eac438d0bfb8cb
jahzlariosa <dev@josephlariosa.com>
2023-07-19 13:06:39 +0800

    Escape Apos

--- app/[...not-found]/page.tsx
+++ app/[...not-found]/page.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
        <div className="flex flex-col items-center justify-center h-[25rem]">
       <h1 className="text-6xl font-bold text-red-500 mb-4 animate-bounce">404</h1>
       <p className="text-xl dark:text-white animate-pulse">
-        Oops! The page you're looking for does not exist.
+        Oops! The page you&apos;re looking for does not exist.
       </p>
       <Link href={'/'} className="bg-zinc-300 hover:bg-zinc-100 dark:text-black font-bold py-2 px-4 rounded mt-4 transition duration-300 ease-in-out">
             Go Back Home
